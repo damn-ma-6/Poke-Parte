35bcc5b32a81982303aaedbe0ba218a4df93f81c
dvicjon <devinvjones@gmail.com>
2021-01-22 12:45:34 -0500

    find pokemon by type

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,263 @@ var currentPokemonEl = document.querySelector(".poke-name");
 var currentTypeEl = document.querySelector(".poke-type");
 var currentImageEl = document.querySelector(".poke-image");
 var currentMoveEl = document.querySelector(".poke-move");
-//var currentHabitatEl = document.querySelector("#pokemon-habitat");
-//var apiURL = "https://pokeapi.co/api/v2/pokemon/"
+
+var getGroundType = function () { //returns 3 random ground type pokemon - display if weather is clear/sunny
+    var apiURL = "https://pokeapi.co/api/v2/type/ground/";
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i=0; i<3; i++) {
+                    console.log(data.pokemon[Math.floor(Math.random() * 20)]);
+                }
+            });
+        } else {
+            alert("Error: " + response.statusText); //statusText property - what the issue is 
+        }
+    })
+};
+
+var getFireType = function() { //returns 3 random fire type pokemon - display if weather is clear/sunny 
+    var apiURL = "https://pokeapi.co/api/v2/type/fire/";
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i=0; i<3; i++) {
+                    console.log(data.pokemon[Math.floor(Math.random() * 20)]);
+                }
+            });
+        } else {
+            alert("Error: " + response.statusText); //statusText property - what the issue is 
+        }
+    })
+};
+
+var getGrassType = function() { //returns 4 random grass type pokemon - display if weather is clear/sunny 
+    var apiURL = "https://pokeapi.co/api/v2/type/grass/";
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i=0; i<4; i++) {
+                    console.log(data.pokemon[Math.floor(Math.random() * 20)]);
+                }
+            });
+        } else {
+            alert("Error: " + response.statusText); //statusText property - what the issue is 
+        }
+    })
+};
+
+var getDarkType = function() { //returns 5 random dark type pokemon - display if weather is foggy 
+    var apiURL = "https://pokeapi.co/api/v2/type/dark/";
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i=0; i<5; i++) {
+                    console.log(data.pokemon[Math.floor(Math.random() * 20)]);
+                }
+            });
+        } else {
+            alert("Error: " + response.statusText); //statusText property - what the issue is 
+        }
+    })
+};
+
+var getGhostType = function() { //returns 5 random ghost type pokemon - display if weather is foggy 
+    var apiURL = "https://pokeapi.co/api/v2/type/ghost/";
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i=0; i<5; i++) {
+                    console.log(data.pokemon[Math.floor(Math.random() * 20)]);
+                }
+            });
+        } else {
+            alert("Error: " + response.statusText); //statusText property - what the issue is 
+        }
+    })
+};
+
+var getIceType = function() { //returns 5 random ice type pokemon - display if weather is snowy 
+    var apiURL = "https://pokeapi.co/api/v2/type/ice/";
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i=0; i<5; i++) {
+                    console.log(data.pokemon[Math.floor(Math.random() * 20)]);
+                }
+            });
+        } else {
+            alert("Error: " + response.statusText); //statusText property - what the issue is 
+        }
+    })
+};
+
+var getSteelType = function() { //returns 5 random steel type pokemon - display if weather is snowy 
+    var apiURL = "https://pokeapi.co/api/v2/type/steel/";
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i=0; i<5; i++) {
+                    console.log(data.pokemon[Math.floor(Math.random() * 20)]);
+                }
+            });
+        } else {
+            alert("Error: " + response.statusText); //statusText property - what the issue is 
+        }
+    })
+};
+
+var getWaterType = function() { //returns 3 random water type pokemon - display if weather is rainy 
+    var apiURL = "https://pokeapi.co/api/v2/type/water/";
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i=0; i<3; i++) {
+                    console.log(data.pokemon[Math.floor(Math.random() * 20)]);
+                }
+            });
+        } else {
+            alert("Error: " + response.statusText); //statusText property - what the issue is 
+        }
+    })
+};
+
+var getElectricType = function() { //returns 3 random electric type pokemon - display if weather is rainy 
+    var apiURL = "https://pokeapi.co/api/v2/type/electric/";
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i=0; i<3; i++) {
+                    console.log(data.pokemon[Math.floor(Math.random() * 20)]);
+                }
+            });
+        } else {
+            alert("Error: " + response.statusText); //statusText property - what the issue is 
+        }
+    })
+};
+
+var getBugType = function() { //returns 4 random bug type pokemon - display if weather is rainy 
+    var apiURL = "https://pokeapi.co/api/v2/type/bug/";
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i=0; i<4; i++) {
+                    console.log(data.pokemon[Math.floor(Math.random() * 20)]);
+                }
+            });
+        } else {
+            alert("Error: " + response.statusText); //statusText property - what the issue is 
+        }
+    })
+};
+
+var getFairyType = function() { //returns 3 random fairy type pokemon - display if weather is cloudy
+    var apiURL = "https://pokeapi.co/api/v2/type/fairy/";
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i=0; i<3; i++) {
+                    console.log(data.pokemon[Math.floor(Math.random() * 20)]);
+                }
+            });
+        } else {
+            alert("Error: " + response.statusText); //statusText property - what the issue is 
+        }
+    })
+};
+
+var getFightingType = function() { //returns 3 random fighting type pokemon - display if weather is cloudy 
+    var apiURL = "https://pokeapi.co/api/v2/type/fighting/";
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i=0; i<3; i++) {
+                    console.log(data.pokemon[Math.floor(Math.random() * 20)]);
+                }
+            });
+        } else {
+            alert("Error: " + response.statusText); //statusText property - what the issue is 
+        }
+    })
+};
+
+var getPoisonType = function() { //returns 4 random poison type pokemon - display if weather is cloudy 
+    var apiURL = "https://pokeapi.co/api/v2/type/poison/";
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i=0; i<4; i++) {
+                    console.log(data.pokemon[Math.floor(Math.random() * 20)]);
+                }
+            });
+        } else {
+            alert("Error: " + response.statusText); //statusText property - what the issue is 
+        }
+    })
+};
+
+var getFlyingType = function() { //returns 3 random flying type pokemon - display if weather is windy
+    var apiURL = "https://pokeapi.co/api/v2/type/flying/";
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i=0; i<3; i++) {
+                    console.log(data.pokemon[Math.floor(Math.random() * 20)]);
+                }
+            });
+        } else {
+            alert("Error: " + response.statusText); //statusText property - what the issue is 
+        }
+    })
+};
+
+var getDragonType = function() { //returns 3 random dragon type pokemon - display if weather is windy
+    var apiURL = "https://pokeapi.co/api/v2/type/dragon/";
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i=0; i<3; i++) {
+                    console.log(data.pokemon[Math.floor(Math.random() * 20)]);
+                }
+            });
+        } else {
+            alert("Error: " + response.statusText); //statusText property - what the issue is 
+        }
+    })
+};
+
+var getPsychicType = function() { //returns 4 random pyschic type pokemon - display if weather is windy
+    var apiURL = "https://pokeapi.co/api/v2/type/dragon/";
+    fetch(apiURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                for (var i=0; i<4; i++) {
+                    console.log(data.pokemon[Math.floor(Math.random() * 20)]);
+                }
+            });
+        } else {
+            alert("Error: " + response.statusText); //statusText property - what the issue is 
+        }
+    })
+};
+
 
 var getPokemon = function() {
     //format the PokeAPI data 
@@ -111,5 +366,21 @@ $(cardEl).on("mouseleave", () => {
 });
 
 
-getPokemon(); 
+getPokemon();
+getGroundType();  
+getFireType(); 
+getGrassType(); 
+getDarkType(); 
+getGhostType(); 
+getIceType(); 
+getSteelType(); 
+getWaterType(); 
+getElectricType(); 
+getBugType(); 
+getFairyType(); 
+getFightingType(); 
+getPoisonType(); 
+getFlyingType(); 
+getDragonType(); 
+getPsychicType(); 
 //getPokemonRegion(); 
