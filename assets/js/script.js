c760db00fbf54b496e2ab8c97d420f5969131eb8
Bethold <mtaycruz@gmail.com>
2021-01-30 15:17:15 -0500

    using different key

--- assets/js/script.js
+++ assets/js/script.js
@@ -93,7 +93,7 @@ var formSubmitHandler = function(event){
 // had to call the city api to get the data key for the city then enter it into the get weather function
 var getCity = function(city){
     
-    var apiUrl = "https://dataservice.accuweather.com/locations/v1/search?apikey=8phV97GIATzlpDJK66fxWSKyzLgvNucC&q=" + city + "&language=en-ca&details=false";
+    var apiUrl = "https://dataservice.accuweather.com/locations/v1/search?apikey=eRaoovk1kImZ6HARsRGM2ycH7vMYA7xt&q=" + city + "&language=en-ca&details=false";
 
     fetch(apiUrl).then(function(response){
         if(response.ok){
@@ -112,7 +112,7 @@ var getCity = function(city){
 // function uses the get hourly weather api from accuweather and uses city key to display weather
 var getWeather = function(cityKey){
 
-    var apiUrl = "https://dataservice.accuweather.com/forecasts/v1/hourly/1hour/" + cityKey + "?apikey=8phV97GIATzlpDJK66fxWSKyzLgvNucC&metric=true";
+    var apiUrl = "https://dataservice.accuweather.com/forecasts/v1/hourly/1hour/" + cityKey + "?apikey=eRaoovk1kImZ6HARsRGM2ycH7vMYA7xt&metric=true";
 
     fetch(apiUrl).then(function(response){
         if(response.ok){
