be2a4469cc86089787cbf1be86be92a7ba7242bb
Neeko Tang <neeko_tvxq@hotmail.com>
2021-01-31 22:38:33 -0500

    update Ali's fixed js

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -159,7 +159,6 @@ for (var i=0; i<pokemonContainerEls.length; i++) {
     let pokeMoveEl = document.createElement("h4");
     pokeMoveEl.innerHTML = moveOne; 
     pokeDiv.append(pokeMoveEl);
-    pokemonContainerEls[i].append(pokeDiv);
-
-}; 
-
+	pokemonContainerEls[i].append(pokeDiv);
+	
+};
